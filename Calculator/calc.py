b15dfc520ba251bb16feab1752fe4287adc4f1e8
Vihar Devalla <vihardevalla@gmail.com>
2019-10-19 23:54:52 +0530

    Fixed ArmstrongNumber,Calculator,ListOps,PrimeFactors,Reciept,Tic-tac-toe
    Fixed ArmstrongNumber,Calculator,ListOps,PrimeFactors,Reciept,Tic-tac-toe

--- Calculator/calc.py
+++ Calculator/calc.py
@@ -1,95 +1,113 @@
-from tkinter import*
+from tkinter import *
 
-me=Tk()
-me.geometry("354x460")
-me.title("CALCULATOR")
-melabel = Label(me,text="CALCULATOR",bg='White',font=("Courier New",30,'bold'))
-melabel.pack(side=TOP)
-me.config(background='Dark gray')
+expression = "" 
 
-textin=StringVar()#nentered by the user
-operator=""#used for the math symbols
 
-def clickbut(number):   #lambda:clickbut(1)
-     global operator
-     operator=operator+str(number)
-     textin.set(operator)
 
-def equlbut():
-     global operator
-     add=double(eval(char(operator)))
-     textin.set(add)
-     operator=''
-def equlbut():
-     global operator
-     sub=double(eval(char(operator)))
-     textin.set(sub)
-     operator=''
-def equlbut():
-     global operator
-     mul=double(eval(char(operator)))
-     textin.set(mul)
-     operator=''
-def equlbut():
-     global operator
-     div=double(eval(char(operator)))
-     textin.set(div)
-     operator=''
+def press(num): 
+	
+	global expression 
+	expression = expression + str(num) 
+	equation.set(expression) 
 
-def clrbut():
-     textin.set('')
-metext=Entry(me,font=("Courier New",12,'bold'),textvar=textin,width=25,bd=5,bg='powder blue')
-metext.pack()
 
-but1=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(1),text="1",font=("Courier New",16,'bold'))
-but1.place(x=10,y=100)
 
-but2=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(2),text="2",font=("Courier New",16,'bold'))
-but2.place(x=10,y=170)
+def equalpress(): 
 
-but3=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(3),text="3",font=("Courier New",16,'bold'))
-but3.place(x=10,y=240)
+	try: 
 
-but4=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(4),text="4",font=("Courier New",16,'bold'))
-but4.place(x=75,y=100)
+		global expression 
+		total = str(eval(expression)) 
+		equation.set(total) 
+		expression = "" 
 
-but5=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(5),text="5",font=("Courier New",16,'bold'))
-but5.place(x=75,y=170)
+	except: 
 
-but6=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(6),text="6",font=("Courier New",16,'bold'))
-but6.place(x=75,y=240)
+		equation.set(" error ") 
+		expression = "" 
 
-but7=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(7),text="7",font=("Courier New",16,'bold'))
-but7.place(x=140,y=100)
 
-but8=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(8),text="8",font=("Courier New",16,'bold'))
-but8.place(x=140,y=170)
 
-but9=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(9),text="9",font=("Courier New",16,'bold'))
-but9.place(x=140,y=240)
+def clear(): 
+	global expression 
+	expression = "" 
+	equation.set("") 
 
-but0=Button(me,padx=14,pady=14,bd=4,bg='white',command=lambda:clickbut(0),text="0",font=("Courier New",16,'bold'))
-but0.place(x=10,y=310)
 
-butdot=Button(me,padx=47,pady=14,bd=4,bg='white',command=lambda:clickbut("."),text=".",font=("Courier New",16,'bold'))
-butdot.place(x=75,y=310)
 
-butpl=Button(me,padx=14,pady=14,bd=4,bg='white',text="+",command=lambda:clickbut("+"),font=("Courier New",16,'bold'))
-butpl.place(x=205,y=100)
+if __name__ == "__main__": 
+	
+	gui = Tk() 
+	gui.configure(background="black") 	
+	gui.title("Calculator") 	
+	gui.geometry("250x450") 	
+	equation = StringVar() 	
+	expression_field = Entry(gui,font=16,bg='black',fg='white',textvariable=equation) 	 
+	expression_field.grid(columnspan=30,ipadx=26,ipady=30) 
+	equation.set('Enter your expression')
+	
+	button1 = Button(gui, text=' 1 ', fg='white', bg='black', 
+					command=lambda: press(1), height=3, width=5) 
+	button1.grid(row=4, column=0) 
 
-butsub=Button(me,padx=14,pady=14,bd=4,bg='white',text="-",command=lambda:clickbut("-"),font=("Courier New",16,'bold'))
-butsub.place(x=205,y=170)
+	button2 = Button(gui, text=' 2 ', fg='white', bg='black', 
+					command=lambda: press(2), height=3, width=5) 
+	button2.grid(row=4, column=1) 
 
-butml=Button(me,padx=14,pady=14,bd=4,bg='white',text="*",command=lambda:clickbut("*"),font=("Courier New",16,'bold'))
-butml.place(x=205,y=240)
+	button3 = Button(gui, text=' 3 ', fg='white', bg='black', 
+					command=lambda: press(3), height=3, width=5) 
+	button3.grid(row=4, column=2) 
 
-butdiv=Button(me,padx=14,pady=14,bd=4,bg='white',text="/",command=lambda:clickbut("/"),font=("Courier New",16,'bold'))
-butdiv.place(x=205,y=310)
+	button4 = Button(gui, text=' 4 ', fg='white', bg='black', 
+					command=lambda: press(4), height=3, width=5) 
+	button4.grid(row=3, column=0) 
 
-butclear=Button(me,padx=14,pady=119,bd=4,bg='white',text="CE",command=clrbut,font=("Courier New",16,'bold'))
-butclear.place(x=270,y=100)
+	button5 = Button(gui, text=' 5 ', fg='white', bg='black', 
+					command=lambda: press(5), height=3, width=5) 
+	button5.grid(row=3, column=1) 
 
-butequal=Button(me,padx=151,pady=14,bd=4,bg='white',command=equlbut,text="=",font=("Courier New",16,'bold'))
-butequal.place(x=10,y=380)
-me.mainloop()
+	button6 = Button(gui, text=' 6 ', fg='white', bg='black', 
+					command=lambda: press(6), height=3, width=5) 
+	button6.grid(row=3, column=2) 
 
+	button7 = Button(gui, text=' 7 ', fg='white', bg='black', 
+					command=lambda: press(7), height=3, width=5) 
+	button7.grid(row=2, column=0) 
+
+	button8 = Button(gui, text=' 8 ', fg='white', bg='black', 
+					command=lambda: press(8), height=3, width=5) 
+	button8.grid(row=2, column=1) 
+
+	button9 = Button(gui, text=' 9 ', fg='white', bg='black', 
+					command=lambda: press(9), height=3, width=5) 
+	button9.grid(row=2, column=2) 
+
+	button0 = Button(gui, text=' 0 ', fg='white', bg='black', 
+					command=lambda: press(0), height=3, width=5) 
+	button0.grid(row=5, column=1) 
+
+	plus = Button(gui, text=' + ', fg='white', bg='#454141', 
+				command=lambda: press("+"), height=3, width=5) 
+	plus.grid(row=2, column=3) 
+
+	minus = Button(gui, text=' - ', fg='white', bg='#454141', 
+				command=lambda: press("-"), height=3, width=5) 
+	minus.grid(row=3, column=3) 
+
+	multiply = Button(gui, text=' * ', fg='white', bg='#454141', 
+					command=lambda: press("*"), height=3, width=5) 
+	multiply.grid(row=4, column=3) 
+
+	divide = Button(gui, text=' / ', fg='white', bg='#454141', 
+					command=lambda: press("/"), height=3, width=5) 
+	divide.grid(row=5, column=2) 
+
+	equal = Button(gui, text=' = ', fg='white', bg='red', 
+				command=equalpress, height=3, width=5) 
+	equal.grid(row=5, column=3) 
+
+	clear = Button(gui, text='Clear', fg='white', bg='#454141', 
+				command=clear, height=3, width=5) 
+	clear.grid(row=5, column=0) 
+
+	gui.mainloop() 
--- Tic-Tac-Toe/tic.py
+++ Tic-Tac-Toe/tic.py
@@ -49,11 +49,11 @@ def btnClick(buttons):
         tkinter.messagebox.showinfo("Tic-Tac-Toe", "Button already Clicked!")
 
 def checkForWin():
-    if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X' or):
+    if (button1['text'] == 'X' and button2['text'] == 'X' and button3['text'] == 'X'):
         disableButton()
         tkinter.messagebox.showinfo("Tic-Tac-Toe", pa)
 
-    elif(flag == 8):
+    elif(flag == 8 and ):
         tkinter.messagebox.showinfo("Tic-Tac-Toe", "It is a Tie")
 
     elif (button1['text'] == 'O' and button2['text'] == 'O' and button3['text'] == 'O' or
@@ -78,31 +78,31 @@ label.grid(row=1, column=0)
 label = Label( tk, text="Player 2:", font='Times 20 bold', bg='white', fg='black', height=1, width=8)
 label.grid(row=2, column=0)
 
-button1 = Button(tk, text=" ", font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button1))
+button1 = Button(tk, text=" ", font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button1))
 button1.grid(row=3, column=0)
 
-button2 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button2))
+button2 = Button(tk, text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button2))
 button2.grid(row=3, column=1)
 
-button3 = Button(tk, text=' ',font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button3))
+button3 = Button(tk, text=' ',font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button3))
 button3.grid(row=3, column=2)
 
-button4 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button4))
+button4 = Button(tk, text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button4))
 button4.grid(row=4, column=0)
 
-button5 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button5))
+button5 = Button(tk, text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button5))
 button5.grid(row=4, column=1)
 
-button6 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button6))
+button6 = Button(tk, text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button6))
 button6.grid(row=4, column=2)
 
-button7 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button7))
+button7 = Button(tk, text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button7))
 button7.grid(row=5, column=0)
 
-button8 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button8))
+button8 = Button(tk, text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button8))
 button8.grid(row=5, column=1)
 
-button9 = Button(tk, text=' ', font='Times 20 bold', bg='gray', fg='white', height=4, width=8, command=lambda: btnClick(button9))
+button9 = Button(tk,text=' ', font='Times 20 bold', bg='black', fg='white', height=4, width=8, command=lambda: btnClick(button9))
 button9.grid(row=5, column=2)
 
 tk.mainloop()
